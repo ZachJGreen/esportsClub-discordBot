c2edae351eb21a7c3baa413a9dd3e59134cd6c8b
Zach Green <112576058+ZachJGreen@users.noreply.github.com>
2023-05-31 11:39:32 -0500

    fixed folder navigation

--- commands/tests/hello.js
+++ commands/tests/hello.js
@@ -1,9 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data : new SlashCommandBuilder().setName('hello').setDescription('Displays "Hello, World!"'),
+    data : new SlashCommandBuilder()
+        .setName('hello')
+        .setDescription('Displays "Hello, World!"'),
     async execute(interaction) {
         await interaction.reply('Hello, World!');
     },
 };
-
